5693ff4ba43ee7a7856cb10bf58e7171516e31f7
Gowtham-S073 <gowtham.kanini@gmail.com>
2023-08-06 15:51:31 +0530

    API Consumption

---
+++ Back-End/TripBooking/MakeYourTrip/Models/TripBookingContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace TripBooking.Models;
+
+public partial class TripBookingContext : DbContext
+{
+    public TripBookingContext()
+    {
+    }
+
+    public TripBookingContext(DbContextOptions<TripBookingContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Booking> Bookings { get; set; }
+
+    public virtual DbSet<Hotel> Hotel { get; set; }
+
+    public virtual DbSet<PackageDetails> PackageDetails { get; set; }
+
+    public virtual DbSet<Package> Package { get; set; }
+
+    public virtual DbSet<Place> Place { get; set; }
+
+    public virtual DbSet<Gallery> Gallery { get; set; }
+
+    public virtual DbSet<RoomBooking> RoomBookings { get; set; }
+
+    public virtual DbSet<RoomDetails> RoomDetails { get; set; }
+
+    public virtual DbSet<RoomType> RoomTypes { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    public virtual DbSet<VehicleBooking> VehicleBookings { get; set; }
+
+    public virtual DbSet<VehicleDetails> VehicleDetails { get; set; }
+
+    public virtual DbSet<Vehicle> Vehicle { get; set; }
+
+    }
